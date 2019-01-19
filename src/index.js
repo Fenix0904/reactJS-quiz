95506609989368f791acb8df169c1e16b81f263b
Svyatoslav Oprysko <opryskosv@gmail.com>
2019-01-19 13:57:19 +0200

    Connected react with redux.

--- src/index.js
+++ src/index.js
@@ -1,43 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
-
-
-const reducer = (state = 0, action) => {
-    switch (action.type) {
-        case "RND":
-            return state + action.payload;
-        case "INC":
-            return state + 1;
-        case "DEC":
-            return state - 1;
-        default:
-            return state;
-    }
-};
-
+import {Provider} from 'react-redux';
+import reducer from "./reducer";
+import App from "./components/app";
 
 const store = createStore(reducer);
 
-const inc = () => ({type: "INC"});
-const dec = () => ({type: "DEC"});
-const rnd = (payload) => ({type: "INC", payload});
-
-document.getElementById("inc")
-    .addEventListener('click', () => {
-        store.dispatch(inc())
-    });
-document.getElementById("dec")
-    .addEventListener('click', () => {
-        store.dispatch(dec())
-    });
-document.getElementById("rnd")
-    .addEventListener('click', () => {
-        const payLoad = Math.floor(Math.random() * 10);
-        store.dispatch(rnd(payLoad));
-    });
-
-const update = () => {
-    document.getElementById("counter")
-        .innerHTML = store.getState();
-};
-
-store.subscribe(update);
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById("root")
+);
