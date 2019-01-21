6684467cd79b16118c76ceef6dc01c083db939af
Fenix0904 <opryskosv@gmail.com>
2019-01-21 15:22:57 +0200

    Implemented action dispatch.

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import './book-list.css';
 import BookListItem from "../book-list-item/book-list-item";
+import {connect} from "react-redux";
+import withBookstoreService from "../hoc/with-bookstore-service";
+import {booksLoaded} from "../../actions";
+import compose from "../../utils/compose";
 
-export default class BookList extends React.Component {
+class BookList extends React.Component {
+
+    componentDidMount() {
+        const {service} = this.props;
+        const data = service.getBooks();
+        this.props.booksLoaded(data);
+    }
 
     render() {
         const {books} = this.props;
@@ -19,3 +29,19 @@ export default class BookList extends React.Component {
         );
     }
 }
+
+
+const mapStateToProps = (state) => {
+    return {
+        books: state.books
+    }
+};
+
+const mapDispatchToProps = {
+    booksLoaded
+};
+
+export default compose(
+    withBookstoreService(),
+    connect(mapStateToProps, mapDispatchToProps)
+)(BookList);
--- src/components/pages/home-page.js
+++ src/components/pages/home-page.js
@@ -2,13 +2,8 @@ import React from 'react';
 import BookList from "../book-list/book-list";
 
 const HomePage = () => {
-    const books = [
-        {id: 1, title: "Test Book 1", author: "Test author 1"},
-        {id: 2, title: "Test Book 2", author: "Test author 2"}
-        ];
-
     return (
-        <BookList books={books}/>
+        <BookList/>
     );
 };
 
