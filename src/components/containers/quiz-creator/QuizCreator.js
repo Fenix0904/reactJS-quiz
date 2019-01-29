e0c7cc31589459fe0bd76d05de1da132f06160e3
Fenix0904 <opryskosv@gmail.com>
2019-01-29 16:35:02 +0200

    Added basic structure of quiz creator page.

--- src/components/containers/quiz-creator/QuizCreator.js
+++ src/components/containers/quiz-creator/QuizCreator.js
@@ -1,10 +1,47 @@
 import React from 'react';
+import './QuizCreator.css';
+import Button from "../../UI/button/Button";
 
 export default class QuizCreator extends React.Component {
+
+    onSubmitHandler = (event) => {
+        event.preventDefault();
+    };
+
+    addQuestionHandler = () => {
+
+    };
+    createQuizHandler = () => {
+
+    };
+
     render() {
         return (
-            <div>
-                QuizCreator
+            <div className='QuizCreator'>
+                <div>
+                    <h1>Create Quiz</h1>
+                    <form onSubmit={this.onSubmitHandler}>
+                        <input/>
+                        <hr/>
+                        <input/>
+                        <input/>
+                        <input/>
+                        <input/>
+                        <select></select>
+                        <Button
+                            type="primary"
+                            onClick={this.addQuestionHandler}
+                        >
+                            Add question
+                        </Button>
+                        <Button
+                            type="correct"
+                            onClick={this.createQuizHandler}
+                        >
+                            Create
+                        </Button>
+                    </form>
+                </div>
             </div>
         );
     }
