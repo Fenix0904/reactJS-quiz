91e7676552eabe44cccc8c28ca5a66456486c143
Fenix0904 <opryskosv@gmail.com>
2019-02-07 14:57:10 +0200

    Connected Quiz list page with firebase.

--- src/components/containers/quiz-list/QuizList.js
+++ src/components/containers/quiz-list/QuizList.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import './QuizList.css';
 import {NavLink} from "react-router-dom";
+import axios from "axios";
 
 export default class QuizList extends React.Component {
 
+    state = {
+        quizes: []
+    };
+
+    async componentDidMount() {
+        try {
+            const response = await axios.get('https://react-quiz-76a7e.firebaseio.com/quizes.json');
+            const quizes = [];
+            Object.keys(response.data).forEach((key, index) => {
+                quizes.push({
+                    id: key,
+                    name: `Test № ${index + 1}`
+                })
+            });
+
+            this.setState({quizes});
+        } catch (e) {
+            console.log(e);
+        }
+
+    }
+
     renderQuizes() {
-        return [1, 2, 4].map((quiz, index) => {
+        return this.state.quizes.map((quiz) => {
             return (
-                <li key={index}>
-                    <NavLink to={'/quiz/' + quiz}>
-                        Test {quiz}
+                <li key={quiz.id}>
+                    <NavLink to={'/quiz/' + quiz.id}>
+                        {quiz.name}
                     </NavLink>
                 </li>
             )
