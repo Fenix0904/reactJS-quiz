721bdb6e4ba0d08a9dbf96ea599ad365aded893f
Fenix0904 <opryskosv@gmail.com>
2019-02-07 15:53:41 +0200

    Connected auth page with firebase.

--- src/components/containers/auth/Auth.js
+++ src/components/containers/auth/Auth.js
@@ -3,6 +3,7 @@ import './Auth.css';
 import Button from "../../UI/button/Button";
 import Input from "../../UI/input/Input";
 import {validate, validateForm} from "../../form/form-framevork";
+import axios from 'axios';
 
 export default class Auth extends React.Component {
 
@@ -36,12 +37,32 @@ export default class Auth extends React.Component {
         }
     };
 
-    loginHandler = () => {
-
+    loginHandler = async () => {
+        const authData = {
+            email: this.state.formControls.email.value,
+            password: this.state.formControls.password.value,
+            returnSecureToken: true
+        };
+        try {
+            const response = await axios.post("https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyAOLW6Oq5DyGRsn8EIKr6IpHlrgTwnNfRs", authData);
+            console.log(response.data);
+        } catch (e) {
+            console.log(e);
+        }
     };
 
-    registerHandler = () => {
-
+    registerHandler = async () => {
+        const authData = {
+            email: this.state.formControls.email.value,
+            password: this.state.formControls.password.value,
+            returnSecureToken: true
+        };
+        try {
+            const response = await axios.post("https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyAOLW6Oq5DyGRsn8EIKr6IpHlrgTwnNfRs", authData);
+            console.log(response.data);
+        } catch (e) {
+            console.log(e);
+        }
     };
 
     submitHandler = (event) => {
