1189201d8ac99d7c567b9afe7e65ac00e5dfae5b
Svyatoslav Oprysko <opryskosv@gmail.com>
2019-02-09 16:29:24 +0200

    Fixed file naming.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Layout from "./hoc/layout/Layout";
 import Quiz from "./containers/quiz/Quiz";
+import {Route, Switch} from "react-router-dom";
+import Auth from "./containers/auth/Auth";
+import QuizCreator from "./containers/quiz-creator/QuizCreator";
+import QuizList from "./containers/quiz-list/QuizList";
 
 const App = () => {
     return (
         <Layout>
-            <Quiz/>
+            <Switch>
+                <Route path='/auth' component={Auth}/>
+                <Route path='/quiz-creator' component={QuizCreator}/>
+                <Route path='/quiz/:id' component={Quiz}/>
+                <Route path='/' component={QuizList}/>
+            </Switch>
         </Layout>
     );
 };
