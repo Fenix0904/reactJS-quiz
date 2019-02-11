d9f932e5209fab2a2112f2931209651818135055
Svyatoslav Oprysko <opryskosv@gmail.com>
2019-02-11 16:08:39 +0200

    Created firs build.

---
+++ build/precache-manifest.29dacc90aca7ce7657b49c0e51aaf5e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "acb98c0125c5185e6f95",
+    "url": "/static/js/main.acb98c01.chunk.js"
+  },
+  {
+    "revision": "254cddd84063012afd12",
+    "url": "/static/js/1.254cddd8.chunk.js"
+  },
+  {
+    "revision": "acb98c0125c5185e6f95",
+    "url": "/static/css/main.52b3bd45.chunk.css"
+  },
+  {
+    "revision": "6d7b61c33e4070ff4498c4b3a9a32ee6",
+    "url": "/index.html"
+  }
+];
